0dbd174465f95fe9fde7464c2b3f1b924150c2e9
Eddie Gurnee <pegurnee@gmail.com>
2015-07-31 17:03:57 -0400

    formatted

--- cookie.js
+++ cookie.js
@@ -2,18 +2,18 @@ var CookieHandler = (function() {
   "use strict";
 
   function getCookieString(key, value) {
-      return encodeURIComponent(key) + '=' + encodeURIComponent(value) + ';';
+    return encodeURIComponent(key) + '=' + encodeURIComponent(value) + ';';
   };
 
-  CookieHandler = function (options) {
+  CookieHandler = function(options) {
     this.theOptions = options;
   }
 
-  CookieHandler.prototype.set = function (key, value) {
+  CookieHandler.prototype.set = function(key, value) {
     document.cookie = getCookieString(key, value);
   }
 
-  CookieHandler.prototype.get = function (key) {
+  CookieHandler.prototype.get = function(key) {
     var indexOfKey, value, encodedKey, lengthOfKey;
 
     encodedKey = encodeURIComponent(key) + '=';
@@ -22,14 +22,14 @@ var CookieHandler = (function() {
     value = '';
 
     if (indexOfKey > -1) {
-        value = decodeURIComponent(document.cookie.substring(indexOfKey + lengthOfKey));
+      value = decodeURIComponent(document.cookie.substring(indexOfKey + lengthOfKey));
     }
     return value;
   }
 
   CookieHandler.prototype.cookie = function(key, value) {
     return ((arguments.length === 1) ?
-            Cookies.get(key) : Cookies.set(key, value));
+      Cookies.get(key) : Cookies.set(key, value));
   }
 
   return CookieHandler;
